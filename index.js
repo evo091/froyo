b02410f66d5492787a33ce309534138af0c6f2a3
Ian <236912195+evo091@users.noreply.github.com>
2026-02-15 13:48:33 -0500

    completed Froyo order assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 let order = prompt("Please enter your desired froyo flavors - separated by commas");
+
+let orderArray = order.split(",");
+    console.log(orderArray)
+
+let flavorCount = {};
+    console.log(flavorCount)
+
+for (let i = 0; i < orderArray.length; ++i) {
+    let currentFlavor = orderArray[i];
+
+    if (!flavorCount[currentFlavor]) {
+        flavorCount[currentFlavor] = 1;
+    }
+    else {
+        ++flavorCount[currentFlavor];
+    }
+    console.log(flavorCount);
+}
